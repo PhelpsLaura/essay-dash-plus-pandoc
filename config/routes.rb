1e4b64b955d5f6fa1aaee0eecbdae3103d3baace
Laura Phelps <laphelps64@gmail.com>
2018-01-10 11:49:16 -0500

    Section views; routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :essays do
+    resources :research_cards
+    resources :sections
+    resources :sources
+  end
+
+  get '/essays/:essay_id/sections/draft_edit/:id', to: 'sections#edit_final', as: 'edit_final_essay_section'
+  root "essays#index"
 end
